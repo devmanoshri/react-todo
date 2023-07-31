6d001dd6036a3c3aac3c2bb3ef8167f204858f1e
devmanoshri <manoshri.no@gmail.com>
2023-07-31 08:52:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const [showEditId, setShowEditId] = useState(0);
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+
+    if (showEditId) {
+      const updatedTodoList = todoList.map((item) =>
+        item.id === showEditId
+          ? { id: showEditId, name: todo }
+          : { id: item.id, name: item.name }
+      );
+      setTodoList(updatedTodoList);
+      setShowEditId(0);
+      setTodo('');
+      return;
+    }
+
+    if (todo !== '') {
+      setTodoList([{ id: Math.random(), name: todo }, ...todoList]);
+      setTodo('');
+    }
+  };
+
+  const handelDelete = (id) => {
+    const newTodoList = todoList.filter((item) => item.id !== id);
+    setTodoList([...newTodoList]);
+    console.log('inside delete');
+  };
+
+  const handelEdit = (id) => {
+    const editTodo = todoList.find((item) => item.id === id);
+    setTodo(editTodo.name);
+    setShowEditId(editTodo.id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Todo List App</h1>
+        <form className="todoForm" onSubmit={handelSubmit}>
+          <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type="submit">{showEditId ? 'Edit' : 'Add'}</button>
+        </form>
+        <ul className="allTodo">
+          {todoList.map((todoItem) => {
+            return (
+              <li className="singleTodo" key={todoItem.id}>
+                <span className="todoText">{todoItem.name}</span>
+
+                <button onClick={() => handelEdit(todoItem.id)}>Edit</button>
+                <button onClick={() => handelDelete(todoItem.id)}>
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
